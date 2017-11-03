1e07dfae6708507f291847e6ade487d09828196f
tungvt45 <33341412+tungvt45@users.noreply.github.com>
2017-11-03 14:59:34 +0700

    Update UpdatePassRoleServlet.java

--- UpdatePassRoleServlet.java
+++ UpdatePassRoleServlet.java
@@ -5,17 +5,7 @@
  */
 package sampie.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.NamingException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import sampie.tblregistrator.tblRegistrationDAO;
+
 
 /**
  *
